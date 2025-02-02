415040928dc26058b819c82b3a7b39305066e2db
中ジョ <nakamura.johnielo@gmail.com>
2025-02-01 21:27:30 -0500

    wordpage, tabありなしでfragment分け。swipeで画面切り替え不可

--- lib/_View/word_page/word_page_fragment.dart
+++ lib/_View/word_page/word_page_fragment.dart
@@ -5,146 +5,126 @@ import 'package:my_dic/DI/product.dart';
 import 'package:my_dic/_View/word_page/conjugacion_fragment.dart';
 import 'package:my_dic/_View/word_page/dictionary_fragment.dart';
 import 'package:my_dic/utils/random.dart';
-import 'package:tuple/tuple.dart';
 
+//input data DS
 class WordPageFragmentInput {
   final int wordId;
   final bool isVerb;
   WordPageFragmentInput({required this.wordId, required this.isVerb});
 }
 
-class WordPageFragment extends StatefulWidget {
+//main fragment
+class WordPageFragment extends StatelessWidget {
   const WordPageFragment({super.key, required this.input});
   final WordPageFragmentInput input;
 
   @override
-  State<WordPageFragment> createState() => _WordPageFragmentState();
+  Widget build(BuildContext context) {
+    if (input.isVerb) {
+      return WordPageWithTab(input: input);
+    }
+    return WordPageWithoutTab(input: input);
+  }
 }
 
-class _WordPageFragmentState extends State<WordPageFragment>
+class WordPageWithTab extends StatefulWidget {
+  const WordPageWithTab({super.key, required this.input});
+  final WordPageFragmentInput input;
+  @override
+  State<WordPageWithTab> createState() => _WordPageWithTabState();
+}
+
+class _WordPageWithTabState extends State<WordPageWithTab>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
+  late PageController _pageController;
+  late PageStorageBucket _bucket;
   late WordPageFragmentInput input;
-  final PageStorageBucket _bucket = PageStorageBucket();
 
   @override
   void initState() {
     super.initState();
-    if (widget.input.isVerb) {
-      _tabController = TabController(length: 2, vsync: this);
-    }
-    input = widget.input;
+    _tabController = TabController(length: 2, vsync: this);
+    _pageController = PageController();
+    _bucket = PageStorageBucket();
     _tabController.addListener(_tabListener);
+    input = widget.input;
   }
 
-  // リスナーを定義
   void _tabListener() {
-    setState(() {
-      // インデックス変更時にウィジェットを再ビルド
-    });
+    if (_tabController.indexIsChanging) {
+      _pageController.animateToPage(
+        _tabController.index,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+    setState(() {});
   }
 
   @override
   void dispose() {
     _tabController.removeListener(_tabListener);
     _tabController.dispose();
+    _pageController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (input.isVerb) {
-      return Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(kToolbarHeight),
-          child: TabBar(
-            controller: _tabController,
-            tabs: [
-              Tab(text: 'Dictionary'),
-              Tab(text: 'Conjugacion'),
-            ],
-          ),
-        ),
-        body: PageStorage(
-          bucket: _bucket,
-          child: //TabBarView(
-              IndexedStack(
-            index: _tabController.index,
-            //コンテンツ内でpadding,margin調整
-            //controller: _tabController,
-            children: [
-              DI<DictionaryFragment>(
-                  param1: WordPageChildInputData(
-                      wordId: input.wordId,
-                      key: PageStorageKey(generateUUID()))),
-              DI<ConjugacionFragment>(
-                  param1: WordPageChildInputData(
-                      wordId: input.wordId,
-                      key: PageStorageKey(generateUUID()))),
-            ],
-          ),
-        ),
-      );
-    }
-
     return Scaffold(
-      body: DI<DictionaryFragment>(
-          param1: WordPageChildInputData(
-              wordId: input.wordId, key: PageStorageKey(generateUUID()))),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: 'Dictionary'),
+            Tab(text: 'Conjugacion'),
+          ],
+        ),
+      ),
+      body: PageStorage(
+        bucket: _bucket,
+        child: IndexedStack(
+          index: _tabController.index,
+          //コンテンツ内でpadding,margin調整
+          //controller: _tabController,
+          children: [
+            DI<DictionaryFragment>(
+                param1: WordPageChildInputData(
+                    wordId: input.wordId, key: PageStorageKey(generateUUID()))),
+            DI<ConjugacionFragment>(
+                param1: WordPageChildInputData(
+                    wordId: input.wordId, key: PageStorageKey(generateUUID()))),
+          ],
+        ),
+      ),
+      /* Stack(
+          children: [
+            PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                _tabController.animateTo(index);
+              },
+              children: [
+                Container(),
+                Container(),
+              ],
+            )
+          ],) */
     );
   }
 }
 
-/* 
-class WordPageFragment extends StatelessWidget {
-  const WordPageFragment({super.key, required this.input});
+class WordPageWithoutTab extends StatelessWidget {
+  const WordPageWithoutTab({super.key, required this.input});
   final WordPageFragmentInput input;
-  static final PageStorageBucket _bucket = PageStorageBucket();
 
   @override
   Widget build(BuildContext context) {
-    if (input.isVerb) {
-      return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: PreferredSize(
-            preferredSize: Size.fromHeight(kToolbarHeight),
-            child: TabBar(
-              tabs: [
-                Tab(text: 'Dictionary'),
-                Tab(text: 'Conjugacion'),
-              ],
-            ),
-          ),
-          body: PageStorage(
-            bucket: _bucket,
-            child: TabBarView(
-              //コンテンツ内でpadding,margin調整
-              children: [
-                DI<DictionaryFragment>(
-                    param1:
-                        Tuple2(input.wordId, PageStorageKey(generateUUID()))),
-                DI<ConjugacionFragment>(
-                    param1:
-                        Tuple2(input.wordId, PageStorageKey(generateUUID()))),
-              ],
-            ),
-          ),
-        ),
-      );
-    }
-
     return Scaffold(
       body: DI<DictionaryFragment>(
-          param1: Tuple2(input.wordId, PageStorageKey(generateUUID()))),
+          param1: WordPageChildInputData(wordId: input.wordId)),
     );
   }
-} */
-
-
-/* 
-Column(children: [
-      MyTabController(),
-      DictionaryFragment(wordId: 120),
-    ]);
- */
+}
